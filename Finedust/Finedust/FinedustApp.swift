a1642f2f253834cfac8a17f6362feaef8ad425b9
joymin5655 <141973483+joymin5655@users.noreply.github.com>
2025-11-03 17:55:09 +0900

    xcode_start

---
+++ Finedust/Finedust/FinedustApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinedustApp.swift
+//  Finedust
+//
+//  Created by JOYMIN on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FinedustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Finedust/FinedustTests/FinedustTests.swift
@@ -0,0 +1,16 @@
+//
+//  FinedustTests.swift
+//  FinedustTests
+//
+//  Created by JOYMIN on 11/3/25.
+//
+
+import Testing
+
+struct FinedustTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

bdc61264ce47835225baaef7e9a2a6385bf2408e
joymin5655 <141973483+joymin5655@users.noreply.github.com>
2025-11-06 16:33:23 +0900

    App_dev_start

---
+++ iOS_App_fd/iOS_App_fd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_App_fd
+//
+//  Created by JOYMIN on 11/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_App_fd/iOS_App_fd/iOS_App_fdApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_App_fdApp.swift
+//  iOS_App_fd
+//
+//  Created by JOYMIN on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_App_fdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
